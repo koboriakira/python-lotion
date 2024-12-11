a367ebc6465ec4f4cc15a15488e05e238e85b838
Kobori Akira <private.beats@gmail.com>
2024-12-11 11:36:25 +0900

    test: 画像ブロックのテストを追加

--- test/test_potion/test_append_block.py
+++ test/test_potion/test_append_block.py
@@ -5,6 +5,8 @@ from block.bulleted_list_item import BulletedlistItem
 from block.callout import Callout
 from block.divider import Divider
 from block.embed import Embed
+from block.heading import Heading
+from block.image import Image
 from block.paragraph import Paragraph
 from potion import Potion
 from properties.text import Text
@@ -40,10 +42,20 @@ class Test(TestCase):
         self._append_block_test(block=divider)
 
     @pytest.mark.post_api()
-    @pytest.mark.current()
     def test_埋め込みを追加する(self):
         embed = Embed.from_url(url="https://www.google.com/")
         self._append_block_test(block=embed)
+
+    @pytest.mark.post_api()
+    def test_見出しを追加する(self):
+        header = Heading.from_plain_text(heading_size=1, text="テスト")
+        self._append_block_test(block=header)
+
+    @pytest.mark.post_api()
+    def test_画像を追加する(self):
+        url = "https://d3swar8tu7yuby.cloudfront.net/IMG_6286_thumb.jpg"
+        image = Image.from_external_url(url=url)
+        self._append_block_test(block=image)
 
     # @pytest.mark.post_api()
     # @pytest.mark.current()
