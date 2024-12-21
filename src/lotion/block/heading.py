97733e1eeee4c584782a32b4f7790141b3f2b166
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-12-21 15:53:24 +0900

    feat: 見出しブロックにリッチテキストを指定可能にする

--- src/lotion/block/heading.py
+++ src/lotion/block/heading.py
@@ -1,21 +1,24 @@
 from .block import Block
 from .rich_text.rich_text import RichText
 
+
 class Heading(Block):
     heading_type: str  # heading_1, heading_2, heading_3
     rich_text: RichText
     color: str | None = None
 
-    def __init__(self,
-                 heading_type: str,
-                 rich_text: RichText,
-                 color: str | None = None,
-                 id: str | None = None,
-                 archived: bool | None = None,
-                 created_time: str | None = None,
-                 last_edited_time: str | None = None,
-                 has_children: bool | None = None,
-                 parent: dict | None = None):
+    def __init__(
+        self,
+        heading_type: str,
+        rich_text: RichText,
+        color: str | None = None,
+        id: str | None = None,
+        archived: bool | None = None,
+        created_time: str | None = None,
+        last_edited_time: str | None = None,
+        has_children: bool | None = None,
+        parent: dict | None = None,
+    ):
         super().__init__(id, archived, created_time, last_edited_time, has_children, parent)
         self.heading_type = heading_type
         self.rich_text = rich_text
@@ -46,6 +49,13 @@ class Heading(Block):
             rich_text=rich_text,
         )
 
+    @staticmethod
+    def from_rich_text(heading_size: int, rich_text: RichText) -> "Heading":
+        return Heading(
+            heading_type=f"heading_{heading_size}",
+            rich_text=rich_text,
+        )
+
     @property
     def type(self) -> str:
         return self.heading_type
@@ -53,7 +63,6 @@ class Heading(Block):
     def to_dict_sub(self) -> dict:
         result = {
             "rich_text": self.rich_text.to_dict(),
-
         }
         if self.color is not None:
             result["color"] = self.color
