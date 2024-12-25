cf9c9b20f2b84a31ca91dc130b6eb2f9c2a0dfd2
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-12-25 17:29:56 +0900

    chore: 不要な実装を削除

--- test/test_lotion/test_api_text.py
+++ test/test_lotion/test_api_text.py
@@ -1,10 +1,8 @@
 import json
-from sys import prefix
 from unittest import TestCase
 
 import pytest
 
-from lotion.block.rich_text.rich_text_builder import RichTextBuilder
 from lotion.properties.text import Text
 from lotion.properties.title import Title
 from test.test_lotion.lotion_utils import create_empty_page, remove_page, update_page
@@ -31,17 +29,8 @@ class TestApiText(TestCase):
         actual = update_page(page=self.page, property=text_empty_prop)
         self.assertEqual(actual.get_text(name="テキスト").text, "")
 
-    @pytest.mark.current
     def test_タイトルを変更する(self) -> None:
         # Given
-        rich_text = (
-            RichTextBuilder.create()
-            .add_text("prefix")
-            .add_page_mention("1596567a3bbf80bb92a0d05094b0c110")
-            .add_text("suffix")
-            .build()
-        )
-        # title = Title.from_rich_text(name="名前", rich_text=rich_text)
         title = Title.from_mentioned_page(
             mentioned_page_id="1596567a3bbf80bb92a0d05094b0c110", prefix="prefix", suffix="suffix"
         )
@@ -51,5 +40,6 @@ class TestApiText(TestCase):
         print(json.dumps(actual.get_title().rich_text.to_dict(), ensure_ascii=False))
         for element in actual.get_title().rich_text.elements:
             print(element.to_plain_text())
+
         # Then
         self.assertEqual(actual.title, "prefixLotion開発用suffix")
