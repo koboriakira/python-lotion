863836b513a6ab3063a5d291a85901ba574ee2c5
Kobori Akira <private.beats@gmail.com>
2025-01-08 10:16:58 +0900

    feat: SelectとMultiSelectを取得する機能にキャッシュを追加

--- test/test_lotion/test_api_select.py
+++ test/test_lotion/test_api_select.py
@@ -2,12 +2,30 @@ from unittest import TestCase
 
 import pytest
 
-from lotion import Lotion
+from lotion import Lotion, notion_prop, notion_database
+from lotion.base_page import BasePage
+from lotion.properties.select import Select
+DATABASE_ID = "15a6567a3bbf80b4a76fc106b37fb92f"
 
+@notion_prop("セレクト")
+class MySelect(Select):
+    pass
+
+
+@notion_database(DATABASE_ID)
+class MyDatabase(BasePage):
+    select: MySelect
 
 @pytest.mark.api()
 class TestApiSelect(TestCase):
-    DATABASE_ID = "15a6567a3bbf80b4a76fc106b37fb92f"
 
     def setUp(self) -> None:
         self.suite = Lotion.get_instance()
+
+    def test_fetch_select(self):
+        result = self.suite.fetch_select(MyDatabase, MySelect, "セレクトA")
+        self.assertEqual(result.selected_name, "セレクトA")
+
+        # キャッシュの確認
+        # result = self.suite.fetch_select(MyDatabase, MySelect, "セレクトA")
+        # self.fail("キャッシュの確認")
